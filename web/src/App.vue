7c38b3c0fad8062d940c8efded2bfdab8cfcff04
Jeroen Claessens <claessej@gmail.com>
2021-07-26 11:55:32 +0200

    initial commit

---
+++ web/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <chart></chart>
+  </div>
+</template>
+
+<script>
+import ECharts from 'chart-wrapper'
+
+export default {
+  name: 'App',
+  components: {
+    chart: ECharts,
+  },
+}
+</script>
+
